a2dd96ffc6c9ca24bd661f2faf78804e53da3e76
Naqeeb Ahmed <Naqeeb532@outlook.com>
2022-01-20 01:51:07 +0530

    BlogSite Setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-
+import React, { useState, useEffect } from 'react';
+import { Layout } from '../Components';
+import 'tailwindcss/tailwind.css'
+import '../styles/globals.scss'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+   </Layout>
+  )
 }
 
 export default MyApp
