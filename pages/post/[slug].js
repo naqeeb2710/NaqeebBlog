14e0daf89d9828a19be34a0679b4c9097a206bab
Naqeeb Ahmed <Naqeeb532@outlook.com>
2022-01-23 22:19:54 +0530

    added loader

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { getPosts,getPostDetails } from '../../services';
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
+import { useRouter } from 'next/router';
 
 const PostDetails = ({post}) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <Loader />;
+  }
   return( 
     <div className="container mx-auto px-10 mb-8">
     <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
