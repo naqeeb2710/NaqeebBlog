3817affda4d1e8e77a9643b4a9b47ae75673b573
Naqeeb Ahmed <Naqeeb532@outlook.com>
2022-02-12 00:35:08 +0530

    chnages for deployment

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -4,11 +4,11 @@ import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loa
 import { useRouter } from 'next/router';
 
 const PostDetails = ({post}) => {
-  const router = useRouter();
+  // const router = useRouter();
 
-  if (router.isFallback) {
-    return <Loader />;
-  }
+  // if (router.isFallback) {
+  //   return <Loader />;
+  // }
   return( 
     <div className="container mx-auto px-10 mb-8">
     <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
@@ -31,7 +31,7 @@ const PostDetails = ({post}) => {
 
 export default PostDetails;
 
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
   const data = await getPostDetails(params.slug);
   return {
     props: {
@@ -42,10 +42,11 @@ export async function getStaticProps({ params }) {
 
 // Specify dynamic routes to pre-render pages based on data.
 // The HTML is generated at build time and will be reused on each request.
-export async function getStaticPaths() {
-  const posts = await getPosts();
-  return {
-    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-    fallback: true,
-  };
-}
+// export async function getStaticPaths() {
+//   const posts = await getPosts();
+//   return {
+//     paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+//     fallback: true,
+//   };
+
+// }
